93488da932affe7ef4385f29ecc077457212afea
svatoslav <kozyyy@yandex.ru>
2017-10-08 09:24:42 +0300

    speech gcp api

--- app/src/main/java/com/idp/emocore/MainActivity.java
+++ app/src/main/java/com/idp/emocore/MainActivity.java
@@ -4,13 +4,19 @@ import android.graphics.Rect;
 import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.List;
 import java.util.Random;
+
+import butterknife.BindView;
 
 
 public class MainActivity extends AppCompatActivity {
